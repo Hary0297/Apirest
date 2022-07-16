4a7c6aa05074d38cd8f2736e5d290d45ae523c29
Hary0297 <109334592+Hary0297@users.noreply.github.com>
2022-07-15 20:53:20 -0500

    first commit

---
+++ routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticuloController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+   Route::post('refresh', [AuthController::class, 'refresh']);
+   // Route::post('me', [AuthController::class, 'me']);
+   // Route::post('register', [AuthController::class, 'register']);
+    //Route::post('register', 'UserController@register');
+});
+Route::post('login', [App\Http\Controllers\Api\LoginController::class, 'login']);
+
+Route::get('/articulos', [ArticuloController::class,'index']); //muestra todos los registros
+Route::post('/articulos', [ArticuloController::class,'store']); // crea un registro
+Route::put('/articulos/{id}', [ArticuloController::class,'update']); // actualiza un registro
+Route::delete('/articulos/{id}', [ArticuloController::class,'destroy']); //elimina un registro
